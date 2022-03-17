334652e15042b35ed6f85a7f6a2d367428c646ca
Patrick Wiseman <patrick@flowerwork.io>
2022-03-17 13:38:11 -0400

    Set base support at elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BuildpacksRegistryApi.MixProject do
     [
       app: :buildpacks_registry_api,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
