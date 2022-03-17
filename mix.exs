93518f56e1c26ca062f349c0cfbfb1cd5b6715f4
Patrick Wiseman <patrick@flowerwork.io>
2022-03-17 14:15:14 -0400

    Add deps and metadata

--- mix.exs
+++ mix.exs
@@ -4,10 +4,15 @@ defmodule BuildpacksRegistryApi.MixProject do
   def project do
     [
       app: :buildpacks_registry_api,
-      version: "0.1.0",
+      description: """
+      An API client for the buildpacks registry with caching.
+      """,
+      deps: deps(),
       elixir: "~> 1.13",
+      homepage_url: "https://github.com/flowerworkio/buildpacks_registry_api",
+      source_url: "https://github.com/flowerworkio/buildpacks_registry_api",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: "0.1.0"
     ]
   end
 
@@ -18,6 +23,9 @@ defmodule BuildpacksRegistryApi.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:httpoison, "~> 1.8"},
+      {:jason, "~> 1.0"}
+    ]
   end
 end
